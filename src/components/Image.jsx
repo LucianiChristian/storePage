9e6da2a0a89efc2d4811f055e48aecc706e725de
Christian Luciani <luciani_christian@yahoo.com>
2022-12-09 21:24:51 -0600

    Create image hover state and condition renders for cart/heart icons

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
 export default function Image({className, img}) {
+    const [hovered, setHovered] = React.useState(false);
+
+    function handleHover() {
+        setHovered(prevState => !prevState);
+    }
+
+    const heartIcon = hovered && <i className="ri-heart-line favorite"></i>;
+    const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>;  
+
     return (
-        <div className={`${className} image-container`}>
-            <img className="image-grid" src={img.url}/>
+        <div className={`${className} image-container` } onMouseLeave={handleHover} onMouseEnter={handleHover}>
+            <img className="image-grid" src={img.url} />
+            {heartIcon}
+            {cartIcon}
         </div>
     )
 }
