5d432c2e7ca1d2633f0316671945606f95b28b34
Christian Luciani <luciani_christian@yahoo.com>
2022-12-13 09:31:25 -0600

    Dynamically render Place Order button based on emptiness of cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -6,9 +6,9 @@ export default function Cart() {
     const { cartItems, cartTotal, emptyCart } = useContext(Context);
     const cartItemElements = cartItems.map(item => <CartItem key={item.id} item={item}/>);
 
-    const totalCostString = cartTotal.toLocaleString("en-US", {style: "currency", currency: "USD"});
-
     const [buttonText, setButtonText] = useState("Place Order"); 
+    
+    const totalCostString = cartTotal.toLocaleString("en-US", {style: "currency", currency: "USD"});
 
     function placeOrder() {
         setButtonText("Ordering...");
@@ -27,10 +27,12 @@ export default function Cart() {
                 <p className="cartItems-cost">Total Cost: {totalCostString}</p>
             </div>
             <div className="cartPage-placeOrderBtnContainer">
-                <button 
-                    className="cartPage-placeOrderBtn"
-                    onClick={ placeOrder }
-                >{buttonText}</button>
+                {cartItems.length > 0 && 
+                    <button 
+                        className="cartPage-placeOrderBtn"
+                        onClick={ placeOrder }
+                    >{buttonText}</button>
+                }
             </div>
         </main>
     )
