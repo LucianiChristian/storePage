2ba4fa74fb2d95bd4644c3a3422a96965581f041
Christian Luciani <luciani_christian@yahoo.com>
2022-12-13 09:41:21 -0600

    Create JS hover handling for delete icon (Cart Item)

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,16 +1,22 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Context } from "../Context";
 
 export default function CartItem({item}) {
     const { removeCartItem } = useContext(Context);
 
+    const [deleteIconHover, setDeleteIconHover] = useState(false);
+    const deleteIconType = deleteIconHover ? "fill" : "line";
+
     return (
         <div className="cartItem">
             <div className="cartItem-iconImageContainer">
-                <i 
-                    className="ri-delete-bin-line"
-                    onClick={() => removeCartItem(item.id)}
-                ></i>
+                <div 
+                    className="deleteIconContainer"
+                    onMouseEnter={() => setDeleteIconHover(true)}
+                    onMouseLeave={() => setDeleteIconHover(false)}
+                >
+                    <i className={`ri-delete-bin-${deleteIconType}`} onClick={() => removeCartItem(item.id)}></i>
+                </div>
                 <img className="cartItem-image" src={item.url}/>
             </div>
             <p className="cartItem-price">$5.00</p>
